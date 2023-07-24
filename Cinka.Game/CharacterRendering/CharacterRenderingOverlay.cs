ec92a8fff6af4d671b9665b5a711a435b5863a18
Cinka <leofoxm40@gmail.com>
2023-07-24 12:46:19 +0300

    Animation update

--- Cinka.Game/CharacterRendering/CharacterRenderingOverlay.cs
+++ Cinka.Game/CharacterRendering/CharacterRenderingOverlay.cs
@@ -4,6 +4,7 @@ using Robust.Shared.Enums;
 using Robust.Shared.IoC;
 using Robust.Shared.Log;
 using Robust.Shared.Maths;
+using Robust.Shared.Timing;
 
 namespace Cinka.Game.CharacterRendering;
 
@@ -11,21 +12,34 @@ public sealed class CharacterRenderingOverlay : Overlay
 {
     [Dependency] private readonly ICharacterManager _characterManager = default!;
     public override OverlaySpace Space => OverlaySpace.WorldSpaceEntities;
-
+    private int _frames = 0;
+    private float _elapsedTime = 0;
+    private float _lastDelta = 0.01f;
+    
     public CharacterRenderingOverlay()
     {
         IoCManager.InjectDependencies(this);
     }
-    
+
+    protected override void FrameUpdate(FrameEventArgs args)
+    {
+        _frames = (_frames + 1) ;
+        _elapsedTime += args.DeltaSeconds;
+        _lastDelta = _elapsedTime / _frames;
+    }
+
     protected override void Draw(in OverlayDrawArgs args)
     {
         var handle = args.WorldHandle;
 
         var lcount = 0f;
-        
         foreach (var character in _characterManager.EnumerateCharacters())
         {
-            var texture = character.Sprite[character.State].Frame0;
+            var sprite = character.Sprite[character.State];
+            var frames = sprite.GetFrames(0);
+            var delay = sprite.GetDelay(_frames % frames.Length);
+            var texture = frames[(int)(_frames*_lastDelta/delay) % frames.Length];
+                
             var prop = (texture.Width / (float)texture.Height);
 
             var charcount = _characterManager.Count();
