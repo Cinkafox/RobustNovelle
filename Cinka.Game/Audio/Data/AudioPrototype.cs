39fafedda574a41009994c0d9c684116175b9a6b
Cinka <leofoxm40@gmail.com>
2023-12-17 21:20:54 +0000

    аудио приколы

--- Cinka.Game/Audio/Data/AudioPrototype.cs
+++ Cinka.Game/Audio/Data/AudioPrototype.cs
@@ -9,8 +9,6 @@ public sealed class AudioPrototype : IPrototype
 {
     [IdDataField] public string ID { get; } = default!;
 
-    //[DataField(required: true)] public string Path = default!;
-
     [DataField] public bool IsBackground;
 
     [DataField("path", required:true, customTypeSerializer:typeof(SoundSpecifierTypeSerializer))] 
--- Cinka.Game/Dialog/DialogActions/AudioAction.cs
+++ Cinka.Game/Dialog/DialogActions/AudioAction.cs
@@ -1,8 +1,10 @@
+using System;
 using Cinka.Game.Audio.Data;
 using Cinka.Game.Dialog.Data;
 using JetBrains.Annotations;
 using Robust.Client.Audio;
 using Robust.Shared.Audio;
+using Robust.Shared.Audio.Components;
 using Robust.Shared.GameObjects;
 using Robust.Shared.IoC;
 using Robust.Shared.Log;
@@ -16,8 +18,15 @@ namespace Cinka.Game.Dialog.DialogActions;
 public sealed partial class AudioAction : IDialogAction
 {
     [DataField("prototype",required:true)] private string _audioPrototype = default!;
+    
+    [DataField("effect")] private string _effectPrototype = String.Empty;
 
     public static EntityUid? Background;
+
+    public static (EntityUid, AudioAuxiliaryComponent) Aux;
+    public static (EntityUid, AudioEffectComponent) Eff;
+
+    public bool first = true;
 
     public void Act()
     {
@@ -30,14 +39,38 @@ public sealed partial class AudioAction : IDialogAction
         var entMan = IoCManager.Resolve<IEntityManager>();
         var audioSystem = entMan.System<AudioSystem>();
 
+        if (first)
+        {
+            Aux = audioSystem.CreateAuxiliary();
+            Eff = audioSystem.CreateEffect();
+        }
+
         if (prototype.IsBackground)
         {
             audioSystem.Stop(Background);
-            Background = audioSystem.PlayGlobal(prototype.Audio, Filter.Local(), true, AudioParams.Default.WithLoop(true).WithVolume(-6)).Value.Entity;
+            Background = PlayAudio(audioSystem,prototype.Audio, AudioParams.Default.WithLoop(true).WithVolume(-6));
         }
         else
         {
-            audioSystem.PlayGlobal(prototype.Audio, Filter.Local(), true, AudioParams.Default);
+            PlayAudio(audioSystem,prototype.Audio, AudioParams.Default);
         }
+
+        first = false;
+    }
+
+    private EntityUid PlayAudio(AudioSystem audioSystem,SoundSpecifier specifier, AudioParams audioParams)
+    {
+        var (uid,comp) =  audioSystem
+            .PlayGlobal(specifier, Filter.Local(), true, audioParams).Value;
+        
+        if (IoCManager.Resolve<IPrototypeManager>().TryIndex<AudioPresetPrototype>(_effectPrototype,out var prototype))
+        {
+            //TODO: изуить поподробнее эту шнягу да
+            audioSystem.SetEffectPreset(Eff.Item1,Eff.Item2,prototype);
+            audioSystem.SetEffect(Aux.Item1,Aux.Item2,Eff.Item1);
+            audioSystem.SetAuxiliary(uid,comp,Aux.Item1);
+        }
+        
+        return uid;
     }
 }
